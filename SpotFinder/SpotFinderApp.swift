0094861912f96dc39f234d61909ef6c9b3d57b19
Gustavo Caldas de Souza <133917173+gustavocaldassouza@users.noreply.github.com>
2025-11-17 16:04:14 -0500

    Initial Commit

---
+++ SpotFinder/SpotFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotFinderApp.swift
+//  SpotFinder
+//
+//  Created by Gustavo Caldas de Souza on 2025-11-17.
+//
+
+import SwiftUI
+
+@main
+struct SpotFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
